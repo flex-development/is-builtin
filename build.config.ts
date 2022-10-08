3098a0c9b942e333c5f7e60b60d60675cebd6bc2
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-10-08 17:23:03 -0400

    build: cleanup build target settings
    - node14 supports es2020
    
    Signed-off-by: Lexus Drumgold <unicornware@flexdevelopment.llc>

--- build.config.ts
+++ build.config.ts
@@ -5,7 +5,6 @@
 
 import { defineBuildConfig, type Config } from '@flex-development/mkbuild'
 import pkg from './package.json' assert { type: 'json' }
-import tsconfig from './tsconfig.build.json' assert { type: 'json' }
 
 /**
  * Build configuration options.
@@ -16,10 +15,7 @@ const config: Config = defineBuildConfig({
   platform: 'node',
   sourcemap: true,
   sourcesContent: false,
-  target: [
-    tsconfig.compilerOptions.target,
-    'node' + pkg.engines.node.replace(/^\D+/, '')
-  ],
+  target: 'node' + pkg.engines.node.replace(/^\D+/, ''),
   treeShaking: true,
   tsconfig: 'tsconfig.build.json'
 })
